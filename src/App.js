3bf277fc1b7ef584d91c4a45857733f204d91b06
Ann Marie Addatu <annmarie.addatu@gmail.com>
2023-10-02 22:08:17 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import SignUp from './Components/Sign_Up/Sign_Up'; // Updated component name
 import Login from './Components/Login/Login';
-import InstantConsultation from './Components/InstantConsultation/InstantConsultation'; // Updated import path
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import { Landing_Page } from './Components/Landing_Page/LandingPage';
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
       <BrowserRouter>
       <Navbar/>
         <Routes>
-          <Route path="/InstantConsultation" element={<InstantConsultation />} />
+          <Route path='/' element={<Landing_Page/>}/>
           <Route path="/Sign_Up" element={<SignUp />} /> 
           <Route path="/Login" element={<Login />} />
+          <Route path="/instant-consultation" element={<InstantConsultation/>} />
         </Routes>
       </BrowserRouter>
     </div>
